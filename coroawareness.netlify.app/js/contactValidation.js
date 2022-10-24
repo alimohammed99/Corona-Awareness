09cb8e819056a308b0e6da8cfdacf919bbb2b0d9
Mohammed Ali <alimoh22062000@gmail.com>
2022-10-24 10:07:58 +0100

    new files

---
+++ coroawareness.netlify.app/js/contactValidation.js
@@ -0,0 +1,12 @@
+function FormVal(){
+
+    var First_Name = getElementById('fname').value;
+    var Last_Name = getElementById('lname').value;
+    var Email = getElementById('email').value;
+    var Subject = getElementById('subject').value;
+    var Message = getElementById('message').value;
+
+      
+
+         
+}
